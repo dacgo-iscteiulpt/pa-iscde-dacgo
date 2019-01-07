115a8d3f12fcd0e81820b2720919feef47a18418
dacgo-iscteiulpt <dacgo@iscte-iul.pt>
2019-01-07 16:37:56 +0000

    Add files via upload
    Modified version of code generator.

---
+++ pt.iscte.pidesco.codegenerator/src/pt/iscte/pidesco/codegenerator/AccessibilityType.java
@@ -0,0 +1,9 @@
+package pt.iscte.pidesco.codegenerator;
+
+/**
+ * ENUM with the purpose of giving the type of accessibility to the statement of code that will be generated.
+ **/
+public enum AccessibilityType {
+	PRIVATE, PROTECTED, PUBLIC
+}
+
